aa283d04f29b516630d1a354ecfe1e6ecd1800fa
Samantha Bretous <sam@samanthabretous.com>
2016-09-26 16:29:18 -0400

    hard coded card objects

---
+++ app/js/cardObj.js
@@ -0,0 +1,191 @@
+// ------------squiggles outline------------
+card55 = {
+  color: "red",
+  shade: "outline",
+  number: 1,
+  shape: "squiggles"
+}
+
+card56 = {
+  color: "red",
+  shade: "outline",
+  number: 2,
+  shape: "squiggles"
+}
+
+card57 = {
+  color: "red",
+  shade: "outline",
+  number: 3,
+  shape: "squiggles"
+}
+
+card58 = {
+  color: "red",
+  shade: "outline",
+  number: 1,
+  shape: "squiggles"
+}
+
+card59 = {
+  color: "green",
+  shade: "outline",
+  number: 2,
+  shape: "squiggles"
+}
+
+card60 = {
+  color: "green",
+  shade: "outline",
+  number: 3,
+  shape: "squiggles"
+}
+
+card61 = {
+  color: "purple",
+  shade: "outline",
+  number: 1,
+  shape: "squiggles"
+}
+
+card62 = {
+  color: "purple",
+  shade: "outline",
+  number: 2,
+  shape: "squiggles"
+}
+
+card63 = {
+  color: "purple",
+  shade: "outline",
+  number: 3,
+  shape: "squiggles"
+}
+
+// ----------------triangle outline---------------
+card64 = {
+  color: "red",
+  shade: "outline",
+  number: 1,
+  shape: "triangle"
+}
+
+card65 = {
+  color: "red",
+  shade: "outline",
+  number: 2,
+  shape: "triangle"
+}
+
+card66 = {
+  color: "red",
+  shade: "outline",
+  number: 3,
+  shape: "triangle"
+}
+
+card67 = {
+  color: "red",
+  shade: "outline",
+  number: 1,
+  shape: "triangle"
+}
+
+card68 = {
+  color: "green",
+  shade: "outline",
+  number: 2,
+  shape: "triangle"
+}
+
+card69 = {
+  color: "green",
+  shade: "outline",
+  number: 3,
+  shape: "triangle"
+}
+
+card70 = {
+  color: "purple",
+  shade: "outline",
+  number: 1,
+  shape: "triangle"
+}
+
+card71 = {
+  color: "purple",
+  shade: "outline",
+  number: 2,
+  shape: "triangle"
+}
+
+card72 = {
+  color: "purple",
+  shade: "outline",
+  number: 3,
+  shape: "triangle"
+}
+
+//------------------oval outline---------------
+card73 = {
+  color: "red",
+  shade: "outline",
+  number: 1,
+  shape: "oval"
+}
+
+card74 = {
+  color: "red",
+  shade: "outline",
+  number: 2,
+  shape: "oval"
+}
+
+card75 = {
+  color: "red",
+  shade: "outline",
+  number: 3,
+  shape: "oval"
+}
+
+card76 = {
+  color: "red",
+  shade: "outline",
+  number: 1,
+  shape: "oval"
+}
+
+card77 = {
+  color: "green",
+  shade: "outline",
+  number: 2,
+  shape: "oval"
+}
+
+card78 = {
+  color: "green",
+  shade: "outline",
+  number: 3,
+  shape: "oval"
+}
+
+card79 = {
+  color: "purple",
+  shade: "outline",
+  number: 1,
+  shape: "oval"
+}
+
+card80 = {
+  color: "purple",
+  shade: "outline",
+  number: 2,
+  shape: "oval"
+}
+
+card81 = {
+  color: "purple",
+  shade: "outline",
+  number: 3,
+  shape: "oval"
+}
