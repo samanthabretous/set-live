c963afd5cd53c4526ab7901a45ab915643712650
Samantha Bretous <sam@samanthabretous.com>
2016-09-26 15:11:51 -0400

    set up environment

---
+++ app/js/app.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+// require("../css/style.css");
+
+var App = React.createClass({
+  render() {
+    return (
+      <h1>Set Game</h1>
+    )
+  }
+})
+
+ReactDOM.render(<App />, document.getElementById('root'))
