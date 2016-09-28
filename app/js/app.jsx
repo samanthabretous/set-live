8b10fb1f6dfa686d4bfc80484c1949a9a205c5ee
Melanie Williams <melaniep518@gmail.com>
2016-09-28 15:35:07 -0400

    function to shuffle deck

--- app/js/app.jsx
+++ app/js/app.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {cards} from "./card_deck.js";
+import {shuffleCards} from './shuffled_deck.js';
 
 // require("../css/style.css");
 
 console.log(cards)
 
-var App = React.createClass({
+const App = React.createClass({
+  getInitialState() {
+    deck: 
+  },
+
   render() {
     return (
       <h1>Set Game</h1>
---
+++ app/js/board.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import {Cards} from './shuffled_deck.js'
+
+const Board = React.createClass({
+
+})
+
+export {Board};
