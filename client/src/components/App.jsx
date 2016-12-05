ab4fbc215d33b7fb2831f44536c419c721a993c9
Samantha Bretous <sam@samanthabretous.com>
2016-12-04 23:28:43 -0500

    load svg. first animation

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import io from 'socket.io-client';
 import store from '../store'
+import SetLogo from '../../images/set_logo.svg';
 
-//import "../css/style.css";
 
 const App = (props) => {
   return (
     <div>
-      <h1>Set Game</h1>
+      <SetLogo />  
       {props.status =='connected' ? <h1>Connected</h1> : null}
       {props.children}
     </div>
