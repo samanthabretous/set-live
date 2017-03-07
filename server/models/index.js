ef4ae3912bacba364629a0566c3d1852323dd138
Samantha Bretous <sam@samanthabretous.com>
2017-03-07 13:23:16 -0500

    add sequelize connection

--- server/models/index.js
+++ server/models/index.js
@@ -5,20 +5,19 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config.json')[env];
 var db        = {};
 
 if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
-    port: match[4],
-    host: match[3],
+    dialectOptions: {
+      ssl: true,
+    },
     logging: true, //false
   });
-}
-
-if (config.use_env_variable) {
+} else if (config.use_env_variable) {
+  var config = require(__dirname + '/../config.json')[env];
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
